226680b24c727d7260502bcf7b6465e7420f95fe
NTA1210 <anhntde180721@fpt.edu.vn>
2025-05-26 09:32:54 +0700

    Lưu thay đổi trước khi chuyển nhánh

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,9 +4,6 @@ export { default as LoginPage } from './Login/Login.js';
 export { default as RegisterPage } from './Register/Register.js';
 export { default as HomePage } from './Home/Home.js';
 export { default as ErrorPage } from './Error/Error.js';
-<<<<<<< HEAD
 
-export { default as DashboardPage } from './DashBoard/Dashboard.js'; 
-=======
+export { default as DashboardPage } from './DashBoard/Dashboard.js';
 export { default as ServiceAndPolicyPage } from './ServiceAndPolicy/index.js';
->>>>>>> origin/develop
