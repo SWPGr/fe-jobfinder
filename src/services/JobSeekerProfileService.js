df5eeb552351ca73e0c968799dc3892527b84fb2
datdeptrainhatthegioi <tiendatyyy2005@gmail.com>
2025-07-06 14:02:25 +0700

    cap nhat trang feedback

--- src/services/JobSeekerProfileService.js
+++ src/services/JobSeekerProfileService.js
@@ -1,11 +1,21 @@
-import { put } from '~/utils/httpRequest';
+import { get, put } from '~/utils/httpRequest';
 
 const updateProfileWithFile = async (formData) => {
     return await put('/profiles', formData, { headers: {} });
 };
 
+const getProfile = async () => {
+    try {
+        const response = await get('/profiles'); // Thay '/profiles' bằng endpoint thực tế (ví dụ: '/profile')
+        return response;
+    } catch (error) {
+        throw error; // Ném lỗi để interceptor xử lý
+    }
+};
+
 const JobSeekerProfileService = {
     updateProfileWithFile,
+    getProfile,
 };
 
 export default JobSeekerProfileService;
